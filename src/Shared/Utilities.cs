decf517ddfac4500b4d3252030edeca9fb75cb45
Jackson Schuster <36744439+jtschuster@users.noreply.github.com>
2025-04-14 12:52:24 -0700

    Update Utilities.cs

--- src/Shared/Utilities.cs
+++ src/Shared/Utilities.cs
@@ -9,7 +9,7 @@ internal static class Utilities
         Span<byte> span = default;
         unsafe
         {
-            span = new Span<byte>((void*)Marshal.AllocHGlobal(1024 * 1024 * 8), 1024 * 1024 * 8);
+            span = new Span<byte>((void*)Marshal.AllocHGlobal(1024 * 32), 1024 * 32);
         }
         return span;
     }
